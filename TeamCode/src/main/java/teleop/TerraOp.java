e8d238a58cd4e38de98245115ba64b5b7813524c
IshaBaj <isha.bajpai11@gmail.com>
2021-08-15 18:23:14 -0700

    move bot gamepad

--- TeamCode/src/main/java/teleop/TerraOp.java
+++ TeamCode/src/main/java/teleop/TerraOp.java
@@ -28,6 +28,7 @@ public class TerraOp extends OpMode {
         // The right stick of gamepad 1 should translate the robot
         // HINT
         // use gamepad1.right_stick_y to get the y value of the right stick
-
+        bot.move(-gamepad1.right_stick_y,gamepad1.right_stick_x,gamepad1.left_stick_x );
     }
 }
+
